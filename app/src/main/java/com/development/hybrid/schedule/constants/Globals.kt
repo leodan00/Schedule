f9000e3375f8996512019f86bd454c3fc7798df0
Leonardo P <leonardo.palma13@est.unanleon.edu.ni>
2021-07-02 14:50:24 -0600

    Adding Constant Class

---
+++ app/src/main/java/com/development/hybrid/schedule/constants/Globals.kt
@@ -0,0 +1,12 @@
+package com.development.hybrid.schedule.constants
+
+object Globals {
+
+    const val TOASTY_TYPE_ERROR: Int = 1
+    const val TOASTY_TYPE_WARNING: Int = 2
+    const val TOASTY_TYPE_INFO: Int = 3
+    const val TOASTY_TYPE_SUCCESS: Int = 4
+    const val TOASTY_TYPE_CUSTOM: Int = 5
+    const val TOASTY_TYPE_NORMAL: Int = 6
+
+}
